47e81c432292e75d132846c4b8fdc1b8d9883a13
Ezgi Nur UCAY <ezginrucay@gmail.com>
2017-12-22 21:15:55 +0300

    Updated Test File name.

--- TestQueue.java
+++ TestQueue.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class TestQueue {
 	public static void main(String[] args) {
-		
 		// TODO Auto-generated method stub
 		Queue fruitQueue = new Queue(100);
 		Queue tempQueue1= new Queue(100); // Temp queues for keeping 5 fruit to  first and secon queue.
