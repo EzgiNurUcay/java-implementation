b45fa49a46e31306f929a2c58f7ad785937fd705
Ezgi Nur UCAY <ezginrucay@gmail.com>
2018-06-02 02:53:31 +0300

    Update Entry.java
    The hash values, K and V are defined.

--- HashTable/Entry.java
+++ HashTable/Entry.java
@@ -1,6 +1,7 @@
 /**
  * Created by Ezgi on 17.11.2017.
  */
+// The aim of this class, creating a struct -protocol- to hash values with abstract data type
 public interface Entry<K, V> {
     K getKey();
     V getValue();
